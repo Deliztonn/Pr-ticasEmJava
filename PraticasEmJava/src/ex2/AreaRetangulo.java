e71af760d338d3ac7ae9a4a1987a9a9c48263e12
yuri <yuri.hdl.tonn@gmail.com>
2025-05-30 14:04:46 -0300

    commit

---
+++ PraticasEmJava/src/ex2/AreaRetangulo.java
@@ -0,0 +1,11 @@
+package ex2;
+
+public class AreaRetangulo {
+
+	public static void main(String[] args) {
+
+		System.out.println("Área de um retângulo\nBase: 10 Altura:7\nÁrea:"+7*10);
+		
+	}
+
+}
---
+++ PraticasEmJava/src/ex4/Media.java
@@ -0,0 +1,12 @@
+package ex4;
+
+public class Media {
+
+	public static void main(String[] args) {
+		double n1 = 3.1;
+		double n2 = 7;
+		double soma = n1 + n2;
+		System.out.println("Soma e média entre 3.2 e 7\nSoma:"+soma+"\nMédia:"+soma/2);
+	}
+
+}
